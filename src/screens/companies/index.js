0e22dbfe4d05e1411e3f0584c1a3ad7d9ec82fa9
Sezai Ozturk <ozturksezai52@gmail.com>
2024-01-02 13:31:24 +0300

    Feature:React native vector icons added

--- src/screens/companies/index.js
+++ src/screens/companies/index.js
@@ -1,11 +1,21 @@
 import {SafeAreaView, Text} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Companies = () => {
     return (
         <SafeAreaView>
-            <Text style={{fontSize: 30, fontFamily: 'Alegreya-Italic'}}>
+            <Text
+                style={{
+                    fontSize: 30,
+                    fontFamily: 'Alegreya-Italic',
+                    color: 'black',
+                }}>
                 Companies Screen
             </Text>
+            <MaterialCommunityIcons
+                name="airplane"
+                style={{fontSize: 40, color: 'green'}}
+            />
         </SafeAreaView>
     );
 };
